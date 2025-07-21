079b5ce71d59dba9d76e5a0d0fa012039d8ebee9
DevendraKumar-JAJ <dkumar06545@gmail.com>
2025-07-21 09:25:38 +0530

    1 to 10 using for loop in php

--- 1_10for.php
+++ 1_10for.php
@@ -1,6 +1,6 @@
 <!--  Write a PHP script to use a while loop to print numbers from 1 to 10. -->
 <?php	
-  for ($i=1;$i<11;$i++){
-    echo "$i ";
+  for ($num=1;$num<11;$num++){
+    echo "$num ";
   }
 ?>

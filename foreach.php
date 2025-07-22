17294b7231ba500770f195e1a1ccab9b97bd42b4
DevendraKumar-JAJ <dkumar06545@gmail.com>
2025-07-22 05:47:03 +0530

    foreach loop

---
+++ foreach.php
@@ -0,0 +1,8 @@
+<!-- 10. Write a PHP script to use a foreach loop to print the elements of an array. -->
+
+<?php	
+  $numArr=array(1,2,3,4,5,6,7,8,9,10);
+  foreach($numArr as $val){
+    echo "$val ";
+  }
+?>

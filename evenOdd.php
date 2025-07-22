2866760b17f8f6a1806a4475248e7b3bf0d6b6fc
DevendraKumar-JAJ <dkumar06545@gmail.com>
2025-07-22 05:46:29 +0530

    A number is odd or even

---
+++ evenOdd.php
@@ -0,0 +1,10 @@
+<!--  Write a PHP script to use an if-else statement to check if a number is even or odd. -->
+  <?php	
+  $num=15;
+  if ($num&1==0){
+    echo "Even";
+  }
+  else{
+    echo "Odd";
+  }
+  ?>

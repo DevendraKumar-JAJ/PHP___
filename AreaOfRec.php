7b7829d0092249ad3a5740cc706c3cd258c5983c
DevendraKumar-JAJ <dkumar06545@gmail.com>
2025-07-22 05:45:42 +0530

    Area of rectangle

---
+++ AreaOfRec.php
@@ -0,0 +1,10 @@
+<!-- 11. Write a PHP function to calculate the area of a rectangle. -->
+
+<?php	
+  function recArea($w,$l){
+    return $w*$l;
+  }
+
+  echo "RecArea : ";
+  echo recArea(4,5);
+?>

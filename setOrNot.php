459bb4c59f7a20ada29b74866ec4f1d0d8df383c
DevendraKumar-JAJ <dkumar06545@gmail.com>
2025-07-22 20:10:47 +0530

    A variable is set a value or not

---
+++ setOrNot.php
@@ -0,0 +1,10 @@
+<!-- 5. Write a PHP script to check if a variable is set or not. -->
+<?php
+  $set;
+  if(isset($set)){
+  echo "Variable is set";
+  }
+  else{
+    echo "Variable is not set." ;
+  }
+?>
